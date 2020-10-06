90f642892c50316ceba6d969127393f7976ab033
Paul Okello <paulokello273@gmail.com>
2020-10-06 13:53:21 +0300

    Nav links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,38 @@
-import React from "react";
-import { Nav, NavbarContainer, NavIcon, NavLogo } from "./Navbar.elements";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import {
+  MobileIcon,
+  Nav,
+  NavbarContainer,
+  NavIcon,
+  NavLogo,
+} from "./Navbar.elements";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
   return (
     <>
-      <Nav>
-        <NavbarContainer>
-          <NavLogo to="/">
-            <NavIcon />
-            KAMNYWESO
-          </NavLogo>
-        </NavbarContainer>
-      </Nav>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav>
+          <NavbarContainer>
+            <NavLogo to="/">
+              <NavIcon />
+              KAMNYWESO
+            </NavLogo>
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+            <NavMenu conClick={handleClick}>
+              <NavItem>
+                <NavLinks to="/">Home</NavLinks>
+              </NavItem>
+            </NavMenu>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
     </>
   );
 };
