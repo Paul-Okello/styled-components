dbead756f1380547edf060617d4b019acfab69c5
Paul Okello <paulokello273@gmail.com>
2020-10-06 11:00:44 +0300

    Global styles

--- src/components/Navbar/Navbar.elements.js
+++ src/components/Navbar/Navbar.elements.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Container } from "../../globalStyles";
 
 export const Nav = styled.nav`
   background: #101522;
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,1 +1,24 @@
 import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*{
+    box-sizing: border-box;
+    margin:0;
+    padding:0;
+    font-family: "Source Sans Pro", sans-serif;
+
+}`;
+export const Container = styled.div`
+  max-width: 1300px;
+  margin-left: auto;
+  margin-left: auto;
+  padding-right: 50px;
+  padding-left: 50px;
+
+  @media screen and (max-width: 991px) {
+    padding-right: 30px;
+    padding-left: 30px;
+  }
+`;
+
+export default GlobalStyle;
